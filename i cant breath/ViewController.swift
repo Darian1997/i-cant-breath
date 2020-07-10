af1a7ebcd4f58f814a5464e816ca2a149022f6a6
Darian Davis <dariandavis788@gmail.com>
2020-07-10 12:29:58 -0500

    This is a one page app

---
+++ i cant breath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i cant breath
+//
+//  Created by Student on 7/10/20.
+//  Copyright © 2020 western health authority. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
